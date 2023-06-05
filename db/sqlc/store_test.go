f9b44625d7d3906bdecb58b61f348e6cf185cc4c
caard0s0 <vinicardoso216@gmail.com>
2023-06-05 18:39:20 -0300

    test(sqlc): Transfer Loan Query

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -164,4 +165,82 @@ func TestTransferTxDeadLock(t *testing.T) {
 	fmt.Println(">> after", updatedAccount1.Balance, updatedAccount2.Balance)
 	require.Equal(t, account1.Balance, updatedAccount1.Balance)
 	require.Equal(t, account2.Balance, updatedAccount2.Balance)
+}
+
+func TestTransferLoan(t *testing.T) {
+	store := NewStore(testDB)
+
+	account := createRandomAccount(t)
+	fmt.Println(">> before", account.Balance)
+
+	// run a concurrent transfer transactions
+	n := 5
+	amount := int64(10)
+	interestRate := int64(1)
+	status := "Active"
+
+	errs := make(chan error)
+	results := make(chan TransferLoanResult)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			ctx := context.Background()
+			result, err := store.TransferLoan(ctx, TransferLoanParams{
+				AccountID:    account.ID,
+				LoanAmount:   amount,
+				InterestRate: interestRate,
+				Status:       status,
+				EndDate:      time.Now().Add(time.Minute),
+			})
+
+			errs <- err
+			results <- result
+		}()
+	}
+
+	// check results
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+		result := <-results
+		require.NotEmpty(t, result)
+
+		// check transfer loan
+		loan := result.Loan
+		require.NotEmpty(t, loan)
+		require.Equal(t, account.ID, loan.AccountID)
+		require.Equal(t, amount, loan.LoanAmount)
+		require.Equal(t, interestRate, loan.InterestRate)
+		require.Equal(t, status, loan.Status)
+		require.NotZero(t, loan.ID)
+		require.NotZero(t, loan.StartDate)
+		require.NotZero(t, loan.EndDate)
+
+		_, err = store.GetLoan(context.Background(), loan.ID)
+		require.NoError(t, err)
+
+		// check entry
+		toEntry := result.ToEntry
+		require.NotEmpty(t, toEntry)
+		require.Equal(t, account.ID, toEntry.AccountID)
+		require.Equal(t, amount, toEntry.Amount)
+		require.NotZero(t, toEntry.ID)
+		require.NotZero(t, toEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), toEntry.ID)
+		require.NoError(t, err)
+
+		// check account
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account.ID, toAccount.ID)
+	}
+
+	// check the final updated balance
+	updatedAccount, err := TestQueries.GetAccount(context.Background(), account.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> after", updatedAccount.Balance)
+	require.Equal(t, account.Balance+int64(n)*amount, updatedAccount.Balance)
 }
