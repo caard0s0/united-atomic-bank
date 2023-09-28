891b9702c10856db781c437a189d22778faf2f9f
Vinicius Cardoso <vinicardoso216@gmail.com>
2023-09-28 11:25:34 -0300

    feat: adding accessToken cookie in a login response (#18)

--- api/server.go
+++ api/server.go
@@ -64,7 +64,8 @@ func corsMiddleware(clientAddress string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", clientAddress)
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Set-Cookie")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
